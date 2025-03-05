37be9f21bc005328ef33dfc2ec94eaeca573853e
Agudelo <agudelo.jhoan.4904@eam.edu.co>
2025-03-05 00:14:13 -0500

    JHOAN SEBASTIAN AGUDELO MEJIA, AGREGANDO METODO C

--- Inventario.java
+++ Inventario.java
@@ -1,9 +1,12 @@
 public class Inventario{
-  
+
     public void metodoB(){
         System.out.println("ejecutando Metodo B");
-
+    }
     public void metodoA(){
         System.out.println("Ejecutando metodo A");
+    }
+    public void metodoC(){
+        System.out.println("Ejecutando metodo C");
     }
 }
