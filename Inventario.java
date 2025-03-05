0a3b24cecfd558aad55a7fec0a41257d8564918a
Juan <pablobedoya3521@gmail.com>
2025-03-05 00:11:45 -0500

    Juan Pablo Bedoya AGREGADO METODO D

--- Inventario.java
+++ Inventario.java
@@ -2,8 +2,13 @@ public class Inventario{
   
     public void metodoB(){
         System.out.println("ejecutando Metodo B");
+    }
 
     public void metodoA(){
         System.out.println("Ejecutando metodo A");
+    }
+
+    public void metodoD(){
+        System.out.println("Ejecutando metodo D");
     }
 }
